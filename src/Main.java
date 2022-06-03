e25734d7f5e4d5332af6657273325064d2f3dbf0
EcaMuntean <vultur_eca@yahoo.com>
2022-06-03 22:55:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ex.1");
+        System.out.println("Hello World! \nMuntean Ecaterina");
+        System.out.println("Ex.2");
+        int x = 10;
+        int y = 15;
+        System.out.println(x + y);
+        System.out.println("Ex.3");
+        int z = 15;
+        double t = 7;
+        System.out.println(z/t);
+        System.out.println("Ex.4");
+        int a = -5;
+        int b = 8;
+        int c = 6;
+        int d = a + b * c;
+
+        System.out.println("a."+ d);
+        int e = 55;
+        int f = 9;
+        int g = 9;
+        int h = (e+f) % g;
+        System.out.println("b."+h);
+        int i = 20;
+        double k = -3;
+        double l = 5;
+        double m = 8;
+        double n = i + k * l /m;
+        System.out.println("c."+ n);
+        int o = 5;
+        double p = 15;
+        int q = 3;
+        int r = 2;
+        int s = 8;
+        int ta = 3;
+        double u = o + p / q * r - s % ta;
+        System.out.println("d." +u);
+
+
+    }
+}
